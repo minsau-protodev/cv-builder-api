e023e8cff4331ed97d057ba8160d767f3c4ae11c
Saúl Gómez Navarrete <minsau@prototipo-devs.com.mx>
2021-05-08 03:22:17 -0500

    Added curriculum endpoints

---
+++ structure/serializers.py
@@ -0,0 +1,16 @@
+import jwt
+from rest_framework import serializers
+from .models import Curriculum
+
+
+class CurriculumSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Curriculum
+        fields = [
+            "uuid",
+            "updated_at",
+            "name",
+            "title",
+            "subtitle",
+            "description",
+        ]
--- structure/views.py
+++ structure/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+from .serializers import CurriculumSerializer
+from .models import Curriculum
 
 # Create your views here.
+class CurriculumViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for create, viewing and editing customer instances.
+    """
+
+    permission_classes = (IsAuthenticated,)
+    serializer_class = CurriculumSerializer
+    queryset = Curriculum.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
