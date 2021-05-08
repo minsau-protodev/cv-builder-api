82b9622df7821c8e963f60f88e5820d455526ed1
Saúl Gómez Navarrete <minsau@prototipo-devs.com.mx>
2021-05-08 03:29:08 -0500

    Added curriculum endpoints

--- structure/serializers.py
+++ structure/serializers.py
@@ -1,4 +1,3 @@
-import jwt
 from rest_framework import serializers
 from .models import Curriculum
 
--- users/serializers.py
+++ users/serializers.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
 from django.contrib.auth import get_user_model
-from rest_framework import serializers
 
 
 class RegisterSerializer(serializers.ModelSerializer):
