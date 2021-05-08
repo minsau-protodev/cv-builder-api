0d9c94d1631dfa8d3c44e2344503ef0b79dac61a
Saul GN <64386242+minsau-protodev@users.noreply.github.com>
2021-05-07 23:21:59 -0500

    Chore/linters precommit (#2)
    * Added precommit configuration
    
    * Solve indent
    
    * Update dependencies
    
    * Added poetry to dependencies

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ('id', 'username')
+        fields = ("id", "username")
